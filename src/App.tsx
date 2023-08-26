a5780b4c6f954aacd2d8c3bf0d88c287f6cd84f1
RenanVidal <102068914+RenanVidal@users.noreply.github.com>
2023-08-26 18:58:13 -0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,35 @@ import { useState } from 'react';
 import { data } from './data';
 import app from './app.module.css';
 import Cards from './Components/Cards';
+import Card from './Components/Cards';
 
 
 function App() {
   
   const [search, setSearch] = useState ('');
+  const [filteredList, setFilteredLid] = useState({});
     
   const listItens = data.map(item => 
     <Cards item={item} /> 
   );
+
+  const handleFilter = (e) => {
+    setSearch(e.currentTarget.value);
+    const filtered = data.filter(list => list.product_name === search);
+    setFilteredLid(filtered);
+  }
 
   return (
     <main className={app.card}>
       <input
         className={app.searchBar}
         value={search}
-        onChange={e => setSearch(e.currentTarget.value)}
+        onChange={handleFilter}
       />
-      {listItens}
+      {search === '' ? listItens : filteredList.map(item =>
+          <Card item={item} />
+        )
+      }
     </main>
   );
 }
