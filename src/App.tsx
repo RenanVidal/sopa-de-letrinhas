7e0d87880318689550696d35bb514ad0128673fd
RenanVidal <102068914+RenanVidal@users.noreply.github.com>
2023-08-29 10:48:57 -0300

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { Children, useState } from 'react';
 import { data } from './data';
 import app from './app.module.css';
 import Card from './Components/Cards';
 
 const dataBases = data.map(item => item);
+const setCategory = new Set();
+const setSubcategory = new Set();
 
-function App() {
+const listCategory = dataBases.filter((listUnique) => {
+  const duplicateInList =setCategory.has(listUnique.product_category);
+  setCategory.add(listUnique.product_category);
+  return !duplicateInList;
+}) 
+
+const listSubcategory = dataBases.filter((listSub) => {
+  const duplicateInListSub = setSubcategory.has(listSub.product_subcategory);
+  setSubcategory.add(listSub.product_subcategory);
+  return !duplicateInListSub;
+})
+
+export default function App() {
   
   const [search, setSearch] = useState ('');
   const [filteredList, setFilteredLid] = useState({});
@@ -23,6 +37,20 @@ function App() {
         value={search}
         onChange={handleFilter}
       />
+      <aside className={app.filters}>
+        <ul className={app.list}>
+          {listCategory.map((item) => {
+            return (
+              <li>
+                <p><input type="checkbox" className={app.chkInp}/>{item.product_category}</p>
+                <ul>
+                  
+                </ul>
+              </li>
+            )
+          })}
+        </ul>
+      </aside>
       {search === '' ? dataBases.map(item =>
         <Card item={item} />
       ) : filteredList.map(item =>
@@ -31,6 +59,3 @@ function App() {
     </main>
   );
 }
-
-export default App;
-
