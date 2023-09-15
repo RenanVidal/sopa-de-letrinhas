6f0b397851e64070551b9945a11e1471ed9b87f5
RenanVidal <102068914+RenanVidal@users.noreply.github.com>
2023-09-14 23:14:19 -0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Children, useState } from 'react';
+import { useState } from 'react';
 import { data } from './data';
 import app from './app.module.css';
 import Card from './Components/Cards';
@@ -9,20 +9,20 @@ const setCategory = new Set(filteredCategories);
 const listCategory = [...setCategory];
 
 export default function App() {
- 
 
   const [search, setSearch] = useState ('');
   const [filteredList, setFilteredList] = useState(dataBases);
-  const [inputChecked, setInputChecked] = useState(null);
+  const [categoryChecked, setCategoryChecked] = useState(null);
+  const [subcategoryChecked, setSubcategoryChecked] = useState(null);
   const [subcategoryItem, setSubcategoryItem] = useState([]);
 
   const handleFilter: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setSearch(event.currentTarget.value);
     if (event.currentTarget.value || event.currentTarget.value === "") {
-      setFilteredList(dataNew);
+      setFilteredList(filteredList);
       return;
     }
-    const filtered = dataNew.filter(list => 
+    const filtered = filteredList.filter(list => 
       list.product_name.toLowerCase().startsWith(
         event.currentTarget.value.toLowerCase()
       )
@@ -30,51 +30,78 @@ export default function App() {
     setFilteredList(filtered);
   }
 
-  const handleFilterMenu: React.MouseEventHandler<HTMLInputElement> = (event) => {
+  const handleFilterCategory: React.MouseEventHandler<HTMLInputElement> = (event) => {
+    
     const clickedFilter = event.currentTarget.value;
-    if (inputChecked !== clickedFilter) { 
+    
+    if (categoryChecked !== clickedFilter) { 
       const newFilter =
       dataBases.filter(newList => newList.product_category.toLowerCase() === clickedFilter.toLowerCase())
       setFilteredList(newFilter);
-      setInputChecked(clickedFilter);
+      setCategoryChecked(clickedFilter);
       const subcategoryItens = dataBases.filter(itens => itens.product_category.toLowerCase() === clickedFilter.toLowerCase());
       const filteredSubcategories = subcategoryItens.map(item => item.product_subcategory);
       const setSubcategory = new Set(filteredSubcategories);
       const listSubcategory = [...setSubcategory]
       setSubcategoryItem(listSubcategory);
     } else { 
-      setInputChecked(null);
+      setCategoryChecked(null);
       setFilteredList(dataBases);
       setSubcategoryItem([]);
     }
+
   }
 
+  const handleFilterSubcategory: React.MouseEventHandler<HTMLInputElement> = (event) => {
+    
+    const clickedFilter = event.currentTarget.value;
+    
+    if (subcategoryChecked !== clickedFilter) { 
+      const newFilter =
+        dataBases.filter(newList => 
+          newList.product_subcategory.toLowerCase() === clickedFilter.toLowerCase());
+      setFilteredList(newFilter);
+      setSubcategoryChecked(clickedFilter);
+    } else { 
+      setSubcategoryChecked(null);
+      setFilteredList(dataBases);
+    }
+
+  }
   /// permitir selecionar um subitem, após selecionado o sub item realizar um novo filtro e exibir na tela  
 
   return (
+    
     <main className={app.card}>
+      
       <input
         className={app.searchBar}
         value={search}
         onChange={handleFilter}
       />
+      
       <aside className={app.filters}>
+      
         <ul className={app.list}>
+      
           <li>Categoria</li>
+      
           {listCategory.map((category) => {
             return (
+              
               <li key={category}>
                 <label>
                   <input 
                     type="checkbox" 
                     className={app.chkInp} 
                     value={category}
-                    checked={inputChecked === category}
-                    onClick={handleFilterMenu}
+                    checked={categoryChecked === category}
+                    onClick={handleFilterCategory}
                   />
                   {category}
                 </label>
-                {inputChecked === category ? 
+                
+                {categoryChecked === category ? 
                   <ul>
                     {subcategoryItem.map((subcategory) =>
                       <label>
@@ -82,22 +109,28 @@ export default function App() {
                           type="checkbox" 
                           className={app.chkInp} 
                           value={subcategory}
-                          checked={inputChecked === subcategory}  
-                          onClick={handleFilterMenu}
+                          checked={subcategoryChecked === subcategory}  
+                          onClick={handleFilterSubcategory}
                         />
                         {subcategory}
                       </label>
                     )}
                   </ul> : null
                 }
+
               </li>
-            )
+            );
           })}
+
         </ul>
+
       </aside>
+      
       {filteredList.map(item =>
         <Card item={item} />
       )}
+
     </main>
+
   );
 }
